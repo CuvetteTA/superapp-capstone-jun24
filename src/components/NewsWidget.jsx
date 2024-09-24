c0bf018309f3bcf963c815635ef8edb79414bf59
Anurag TA@Cuvette <anurag@cuvette.tech>
2024-09-24 22:05:45 +0530

    Optimization: Network Calls Minimized by using cache

--- src/components/NewsWidget.jsx
+++ src/components/NewsWidget.jsx
@@ -5,31 +5,37 @@ import formatDateAndTime from "../utils/formatDateAndTime";
 
 const NewsWidget = () => {
   const [news, setNews] = useState();
+
   useEffect(() => {
     fetchNews().then((data) => {
       if (data.status == "ok") {
-        const randomIndex = Math.floor(
-          Math.random() * data.articles.length
-        );
+        const randomIndex = Math.floor(Math.random() * data.articles.length);
         setNews(data.articles[randomIndex]);
       }
     });
-  },[]);
+  }, []);
 
-  console.log(news);
+  const renderPublishedAt = (timestamp) => {
+    const { date, time } = formatDateAndTime(timestamp);
+    return `${date} | ${time}`;
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <img src={news?.urlToImage} alt="News Image" />
-        <div>
-          <p>{news?.title}</p>
-          <p>{news?.publishedAt}</p>
+
+
+      <>
+        <div className={styles.header}>
+          <img src={news?.urlToImage} alt="News Image" />
+          <div>
+            <p>{news?.title}</p>
+            <p>{news?.publishedAt}</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.body}>
-        <p>{news?.content}</p>
-      </div>
+        <div className={styles.body}>
+          <p>{news?.content}</p>
+        </div>
+      </>
     </div>
   );
 };
