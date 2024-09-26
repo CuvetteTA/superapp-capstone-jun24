f401e4db2d80b88e376a36e7c1af790edd762f44
Nandit <nanditsareria@gmail.com>
2024-09-27 02:10:57 +0530

    Added: Skeleton Loader Component Styled: News Widget, Profile Widget, Weather Widget.

--- src/components/NewsWidget.jsx
+++ src/components/NewsWidget.jsx
@@ -1,41 +1,55 @@
 import React, { useState, useEffect } from "react";
 import fetchNews from "../api/fetchNews";
 import styles from "./NewsWidget.module.css";
-import formatDateAndTime from "../utils/formatDateAndTime";
+import SkeletonLoader from "./SkeletonLoader";
+import moment from "moment";
 
 const NewsWidget = () => {
   const [news, setNews] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchNews().then((data) => {
-      if (data.status == "ok") {
+      if (data.status === "ok") {
         const randomIndex = Math.floor(Math.random() * data.articles.length);
         setNews(data.articles[randomIndex]);
+        setLoading(false);
       }
     });
   }, []);
 
-  const renderPublishedAt = (timestamp) => {
-    const { date, time } = formatDateAndTime(timestamp);
-    return `${date} | ${time}`;
-  };
-
   return (
     <div className={styles.container}>
-
-
-      <>
-        <div className={styles.header}>
-          <img src={news?.urlToImage} alt="News Image" />
-          <div>
-            <p>{news?.title}</p>
-            <p>{news?.publishedAt}</p>
-          </div>
-        </div>
+      <div className={styles.imageContainer}>
+        {loading ? (
+          <SkeletonLoader height="200px" />
+        ) : (
+          <>
+            <img
+              className={styles.image}
+              src={news?.urlToImage}
+              alt="News Image"
+            />
+            <div className={styles.titleOverlay}>
+              <p>{news?.title}</p>
+              {news?.publishedAt && (
+                <p className={styles.publishedDate}>
+                  {moment(news.publishedAt).format("DD:MM:YYYY")}
+                </p>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+      <div className={styles.contentContainer}>
         <div className={styles.body}>
-          <p>{news?.content}</p>
+          {loading ? (
+            <SkeletonLoader count={2} height="20px" />
+          ) : (
+            <p>{news?.content}</p>
+          )}
         </div>
-      </>
+      </div>
     </div>
   );
 };
